94d75b1751a32cbef9d22bd1c49fd8ce55191f32
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-13 20:47:12 +0000

    kata 7 with filter and ternary functions

---
+++ kata7_inTheAir.js
@@ -0,0 +1,21 @@
+const checkAir = function(samples, threshold) {
+  const cleanCount = samples.filter(sample => sample === 'clean').length;
+  const dirtyCount = samples.filter(sample => sample === 'dirty').length;
+
+  return (dirtyCount / cleanCount) <= threshold ? 'Clean' : 'Polluted';
+};
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+));
